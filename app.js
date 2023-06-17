0f558ff8b8763419d024e01c962981aa9c9095a3
ghostmodd <ghost1mod@gmail.com>
2023-06-17 04:24:31 +0300

    fix: added app.js file

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const bodyParser = require('body-parser');
 const { cardsRouter } = require('./routes/cards');
 const { usersRouter } = require('./routes/users');
 
-const index = express();
+const app = express();
 const { PORT = 3001 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-index.use((req, res, next) => {
+app.use((req, res, next) => {
   req.user = {
     _id: '648900d021ef92f08b97d618',
   };
   next();
 });
 
-index.use(bodyParser.json());
-index.use(bodyParser.urlencoded({ extended: true }));
-index.use('/cards', cardsRouter);
-index.use('/users', usersRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/cards', cardsRouter);
+app.use('/users', usersRouter);
 
-index.listen(PORT, 'localhost');
+app.listen(PORT, 'localhost');
