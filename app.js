dec3da53863beabd569faf549bcb13fb9343a638
ghostmodd <ghost1mod@gmail.com>
2023-07-09 05:13:48 +0300

    trying to fix mongod connection v. 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const indexRouter = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 mongoose.connect(DB_URL);
 
 app.use(cookieParser());
