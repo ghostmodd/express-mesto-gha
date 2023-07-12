2fc19dcca177041c2da7fae429250073c1865fa7
ghostmodd <ghost1mod@gmail.com>
2023-07-12 03:46:17 +0300

    cors handle

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(reqLogger);
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   next();
 });
 app.use('/', indexRouter);
