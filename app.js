addc3f7615574f575764bc47fd1284a80c0389a2
ghostmodd <ghost1mod@gmail.com>
2023-07-13 02:30:41 +0300

    fix: handle CORS 2

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const corsHandling = require('./middlewares/corsHandling');
 const indexRouter = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const { reqLogger, errLogger } = require('./middlewares/logger');
 
 const app = express();
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 mongoose.connect(DB_URL);
 
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 app.use(reqLogger);
+app.use(corsHandling);
 app.use('/', indexRouter);
 app.use(errLogger);
 app.use(errors());
