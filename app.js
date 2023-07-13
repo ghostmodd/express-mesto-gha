ff7a82151b53acd93413689b2c95602826e15f88
ghostmodd <ghost1mod@gmail.com>
2023-07-13 15:32:30 +0300

    feat: crash test and JWT storage in env file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const { reqLogger, errLogger } = require('./middlewares/logger');
 
 const app = express();
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb', JWT_SECRET = 'simpleKey' } = process.env;
 mongoose.connect(DB_URL);
 
 const allowedCors = [
